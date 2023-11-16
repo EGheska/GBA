c09e20498126a5945e15e73f6b48e497d2e056a6
EGheska <dimanich300@gmail.com>
2023-11-16 15:26:12 +0100

    Initial design was added to the main frame

--- src/gui/settings_menu.gd
+++ src/gui/settings_menu.gd
@@ -1,5 +1,7 @@
 extends Popup
 
+@onready var settings_menu = $SettingsMenu
+
 # General Settings
 @onready var display_options = $SettingsTab/General/MarginContainer/GeneralSettings/DisplayModeButton
 @onready var sound_options = $SettingsTab/General/MarginContainer/GeneralSettings/CheckButton
@@ -8,6 +10,8 @@ extends Popup
 @onready var help_button = $SettingsTab/Gameplay/MarginContainer/GameplaySettings/HelpCheckButton
 # Account Settings
 @onready var skills_menu = $SettingsTab/Account/MarginContainer/GeneralSettings/SkillsmenuButton
+
+
 
 func _ready():
 	pass
@@ -24,8 +28,10 @@ func _on_display_mode_button_item_selected(index):
 func _on_check_button_toggled(button_pressed):
 	if !button_pressed:
 		AudioServer.set_bus_volume_db(0, -72)
+		print("Volume set to 0")
 	else:
 		AudioServer.set_bus_volume_db(0, 0)
+		print("Volume set to 100")
 
 
 func _on_difficulty_mode_button_item_selected(index):
