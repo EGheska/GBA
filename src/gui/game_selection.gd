79087cac966e9b50465d17a2c18ff652ca94c980
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2023-11-17 10:10:24 +0100

    Settings now available from game select menu

--- src/gui/game_selection.gd
+++ src/gui/game_selection.gd
@@ -1,8 +1,18 @@
 extends CanvasLayer
 
 @onready var ThisTile = $"."
+@onready var SettingsMenu = $"SettingsMenu"
 
 
 func _on_back_button_pressed():
 	if ThisTile.is_visible():
 		ThisTile.set_visible(false)
+
+
+func _on_settings_button_pressed():
+	if !SettingsMenu.is_visible():
+		SettingsMenu.set_visible(true)
+		SettingsMenu.popup_centered()
+	else:
+		SettingsMenu.set_visible(false)
+		
