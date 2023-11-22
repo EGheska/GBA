4c0d58173f4a12ef6e96c6c7a44f54127ec6f62e
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2023-11-22 13:32:00 +0100

    small fixes?

--- src/gui/main_menu.gd
+++ src/gui/main_menu.gd
@@ -3,33 +3,28 @@ extends CanvasLayer
 @onready var SettingsMenu = $SettingsMenu
 @onready var GameSelection = $GameSelection
 
-@onready var MarginContainers = $Menu/MarginContainer/VBoxContainer/MarginBetween
+@onready var MarginContainers = $Menu/MarginContainer/VBoxContainer/MarginBetween2
+
+
 
 
 func _ready():
 	pass
 
-func _on_settings_button_toggled(button_pressed):
+
+func _on_start_button_pressed():
+	if !GameSelection.is_visible():
+		GameSelection.set_visible(true);
+	
+
+
+func _on_settings_button_pressed():
 	if SettingsMenu.is_visible():
 		SettingsMenu.set_visible(false)
 	else:
 		SettingsMenu.set_visible(true)
 		SettingsMenu.popup_centered()
-	
 
 
-func _on_quit_button_toggled(button_pressed):
+func _on_quit_button_pressed():
 	get_tree().quit() 
-
-
-func _on_start_button_pressed():
-	if !GameSelection.is_visible():
-		GameSelection.set_visible(true);
-		
-
-
-func _on_menu_resized():
-	var mode = DisplayServer.window_get_mode()
-	if mode == 3:
-		MarginContainers.add_theme_constant_override("margin_bottom", 100)
-
--- src/gui/settings_menu.gd
+++ src/gui/settings_menu.gd
@@ -5,22 +5,25 @@ extends Popup
 # General Settings
 @onready var display_options = $SettingsTab/General/MarginContainer/GeneralSettings/DisplayModeButton
 @onready var sound_options = $SettingsTab/General/MarginContainer/GeneralSettings/CheckButton
-# Gameplay Settings
-@onready var difficulty_mode = $SettingsTab/Gameplay/MarginContainer/GameplaySettings/DifficultyModeButton
-@onready var help_button = $SettingsTab/Gameplay/MarginContainer/GameplaySettings/HelpCheckButton
+# Advanced Settings
+@onready var Dyslexia = $SettingsTab/Advanced/MarginContainer/AdvanvedSettings/DyslexiaButton
+
+
 # Account Settings
 @onready var skills_menu = $SettingsTab/Account/MarginContainer/GeneralSettings/SkillsmenuButton
 @onready var PopUp = $"."
+
+
+
 
 func _ready():
 	pass
 	
 
-func _on_display_mode_button_item_selected(index):		
+func _on_display_mode_button_item_selected(index):	
 	if index == 0:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		PopUp.popup_centered()
-		display_options.select(index)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		PopUp.popup_centered()
@@ -35,3 +38,5 @@ func _on_sound_button_toggled(button_pressed):
 	else:
 		AudioServer.set_bus_volume_db(0, 0)
 		print("Volume set to 100")
+
+
