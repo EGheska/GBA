d90d137c8d5427fce9f2aabd1cfa986183cb52cc
EGheska <dimanich300@gmail.com>
2023-11-23 11:43:57 +0100

    buttons fixed

--- src/gui/game_selection.gd
+++ src/gui/game_selection.gd
@@ -3,15 +3,21 @@ extends CanvasLayer
 @onready var ThisTile = $"."
 @onready var SettingsMenu = $"SettingsMenu"
 
+@onready var BackButton = $GameSelection/MarginContainer/VBoxContainer/BackButton
+@onready var SettingButton = $GameSelection/MarginContainer/VBoxContainer/SettingsButton
+
 
 func _on_back_button_pressed():
 	if ThisTile.is_visible():
 		ThisTile.set_visible(false)
+		BackButton.set_pressed_no_signal(false)
 
 
 func _on_settings_button_pressed():
 	if !SettingsMenu.is_visible():
 		SettingsMenu.set_visible(true)
 		SettingsMenu.popup_centered()
+		SettingButton.set_pressed_no_signal(false)
 	else:
 		SettingsMenu.set_visible(false)
+		SettingButton.set_pressed_no_signal(false)
--- src/gui/main_menu.gd
+++ src/gui/main_menu.gd
@@ -5,6 +5,10 @@ extends CanvasLayer
 
 @onready var MarginContainers = $Menu/MarginContainer/VBoxContainer/MarginBetween2
 
+@onready var StartButton = $Menu/MarginContainer/VBoxContainer/StartButton
+@onready var SettingButton = $Menu/MarginContainer/VBoxContainer/SettingsButton
+@onready var QuitButton = $Menu/MarginContainer/VBoxContainer/QuitButton
+
 
 
 
@@ -15,16 +19,20 @@ func _ready():
 func _on_start_button_pressed():
 	if !GameSelection.is_visible():
 		GameSelection.set_visible(true);
+		StartButton.set_pressed_no_signal(false)
 	
 
 
 func _on_settings_button_pressed():
 	if SettingsMenu.is_visible():
 		SettingsMenu.set_visible(false)
+		SettingButton.set_pressed_no_signal(false)
 	else:
 		SettingsMenu.set_visible(true)
+		SettingButton.set_pressed_no_signal(false)
 		SettingsMenu.popup_centered()
 
 
 func _on_quit_button_pressed():
 	get_tree().quit() 
+	QuitButton.set_pressed_no_signal(false)
