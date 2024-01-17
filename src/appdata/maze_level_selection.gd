8da312b8d8a7a0aaee6c8b234468289919bd5a83
Dmytro Taras <48064427+EGheska@users.noreply.github.com>
2024-01-17 09:28:06 +0100

    Fixes with the maze

--- src/appdata/maze_level_selection.gd
+++ src/appdata/maze_level_selection.gd
@@ -56,15 +56,15 @@ func _ready():
 		pass
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://src/appdata/maze1intro.tscn")
+	get_tree().change_scene_to_file("res://src/appdata/maze_game.tscn")
 
 
 func _on_button_2_pressed():
-		get_tree().change_scene_to_file("res://src/appdata/maze2intro.tscn")
+		get_tree().change_scene_to_file("res://src/appdata/maze_game_lvl2.tscn")
 
 		
 func _on_button_4_pressed():
-		get_tree().change_scene_to_file("res://src/appdata/maze3_intro.tscn")
+		get_tree().change_scene_to_file("res://src/appdata/maze_lvl3.tscn")
 
 
 
